58638b9149471935ed5e93adff8f4c850b722733
xaedes <xaedes@gmail.com>
2022-03-08 17:39:42 +0100

    Refactor so3_math.h code

--- FAST_LIO_cpp/include/FAST_LIO_cpp/so3_math.h
+++ FAST_LIO_cpp/include/FAST_LIO_cpp/so3_math.h
@@ -4,13 +4,15 @@
 #include <math.h>
 #include <Eigen/Core>
 
-#define SKEW_SYM_MATRX(v) 0.0,-v[2],v[1],v[2],0.0,-v[0],-v[1],v[0],0.0
+namespace fast_lio { 
+
+#define FAST_LIO_FAST_LIO_SKEW_SYM_MATRX(v) 0.0,-v[2],v[1],v[2],0.0,-v[0],-v[1],v[0],0.0
 
 template<typename T>
 Eigen::Matrix<T, 3, 3> skew_sym_mat(const Eigen::Matrix<T, 3, 1> &v)
 {
     Eigen::Matrix<T, 3, 3> skew_sym_mat;
-    skew_sym_mat<<0.0,-v[2],v[1],v[2],0.0,-v[0],-v[1],v[0],0.0;
+    skew_sym_mat << FAST_LIO_FAST_LIO_SKEW_SYM_MATRX(v);
     return skew_sym_mat;
 }
 
@@ -23,7 +25,7 @@ Eigen::Matrix<T, 3, 3> Exp(const Eigen::Matrix<T, 3, 1> &&ang)
     {
         Eigen::Matrix<T, 3, 1> r_axis = ang / ang_norm;
         Eigen::Matrix<T, 3, 3> K;
-        K << SKEW_SYM_MATRX(r_axis);
+        K << FAST_LIO_SKEW_SYM_MATRX(r_axis);
         /// Roderigous Tranformation
         return Eye3 + std::sin(ang_norm) * K + (1.0 - std::cos(ang_norm)) * K * K;
     }
@@ -44,7 +46,7 @@ Eigen::Matrix<T, 3, 3> Exp(const Eigen::Matrix<T, 3, 1> &ang_vel, const Ts &dt)
         Eigen::Matrix<T, 3, 1> r_axis = ang_vel / ang_vel_norm;
         Eigen::Matrix<T, 3, 3> K;
 
-        K << SKEW_SYM_MATRX(r_axis);
+        K << FAST_LIO_SKEW_SYM_MATRX(r_axis);
 
         T r_ang = ang_vel_norm * dt;
 
@@ -66,7 +68,7 @@ Eigen::Matrix<T, 3, 3> Exp(const T &v1, const T &v2, const T &v3)
     {
         T r_ang[3] = {v1 / norm, v2 / norm, v3 / norm};
         Eigen::Matrix<T, 3, 3> K;
-        K << SKEW_SYM_MATRX(r_ang);
+        K << FAST_LIO_SKEW_SYM_MATRX(r_ang);
 
         /// Roderigous Tranformation
         return Eye3 + std::sin(norm) * K + (1.0 - std::cos(norm)) * K * K;
@@ -107,5 +109,9 @@ Eigen::Matrix<T, 3, 1> RotMtoEuler(const Eigen::Matrix<T, 3, 3> &rot)
     Eigen::Matrix<T, 3, 1> ang(x, y, z);
     return ang;
 }
+
+#undef FAST_LIO_SKEW_SYM_MATRX
+
+} // namespace fast_lio
 
 #endif
